ffd4710fc61d26ecd55060b715a225e5897b67f7
dbeken <dbeken@blackned.de>
2022-07-24 22:53:43 +0200

    from stephen

--- src/App.test.js
+++ src/App.test.js
@@ -7,19 +7,7 @@ test("renders learn react link", () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test("renders learn react link2", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test("renders learn react link3", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test("renders learn react link4", () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
